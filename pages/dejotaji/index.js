19e15b67b42491c7cae57b50f4ab35241cb17c69
Valdis Meirens <valdismeirens@gmail.com>
2024-08-29 01:47:57 +0300

    rotalacredentials

--- pages/dejotaji/index.js
+++ pages/dejotaji/index.js
@@ -5,23 +5,23 @@ import styles from "./dancers.module.css";
 import Footer from "@/components/footer/footer";
 import { server } from "@/components/config/constants";
 
-export default function Dancers({ data }) {
+export default function Dancers() {
   return (
     <div className={styles.container}>
       <DancersHeader />
-      <DancersContainer dancers={data} />
+      <DancersContainer />
       <Footer />
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  const res = await fetch(`${server}/api/dancers`);
-  const data = await res.json();
+// export async function getServerSideProps() {
+//   const res = await fetch(`${server}/api/dancers`);
+//   const data = await res.json();
 
-  return {
-    props: {
-      data,
-    },
-  };
-}
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
