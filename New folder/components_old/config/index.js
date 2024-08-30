5aa5dbf453aa40c1d00af6b79a9a0f94d08d4d28
Valdis Meirens <valdismeirens@gmail.com>
2024-08-30 19:58:41 +0300

    old

--- New folder/components_old/config/index.js
+++
@@ -1,4 +0,0 @@
-const dev = process.env.NODE_ENV !== "production";
-
-export const server = dev ? "https://bdkdrellisi.lv" : "https://bdkdrellisi.lv";
-// export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
